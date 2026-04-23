c651f991d2e29ad5ce7485b5420af57d9fe01cc0
pkfarado <pkfarado@cmc.pshs.edu.ph>
2026-04-23 10:03:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+Quarter_1_grade=float(input("Please Enter Your Tentative Grade for Quarter 1: "))
+Quarter_2_grade=float(input("Please Enter Your Tentative Grade for Quarter 2: "))
+Quarter_3_grade=float(input("Please Enter Your Tentative Grade for Quarter 3: "))
+Quarter_4_grade=float(input("Please Enter Your Tentative Grade for Quarter 4: "))
+q2=(Quarter_1_grade + 2*Quarter_2_grade) / 3
+q3=(Quarter_2_grade + 2*Quarter_3_grade) / 3
+Final_grade= (q3 + 2*Quarter_4_grade) / 3
+print("Your Final Grade is: ", Final_grade)
